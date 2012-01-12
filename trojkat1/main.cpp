af2f9d4d5936fb58b1ba0e6b36f4bf07c4d62f6a
KarinaSzewczyk <karina.szewczyk@poczta.onet.pl>
2012-01-12 19:17:32 +0100

    Update trojkat1/main.cpp

--- trojkat1/main.cpp
+++ trojkat1/main.cpp
@@ -7,13 +7,13 @@ int main()
     int m;
     cout << "Wczytaj liczbe: \n";
     cin >> m;
-    for(int i=1; i <= m; i++)
+    for(int i = 0; i < m; i++)
     {
 
-        for(int j = m; j <= i ; j++)
-        {
-            cout << "*";
-        }
+        for(int j = 0; j < m - i -1 ; j++)
+           cout << ' ';
+        for(int j = 0; j < 2 * i + 1; j++
+            cout << '*';
         cout << endl;
     }
 
